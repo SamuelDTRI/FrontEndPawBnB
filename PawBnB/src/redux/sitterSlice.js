7a7c0fa91cf85d47e015e66a42dba9b36aa42bab
jOsterrielt <jorgeosterrielt@gmail.com>
2024-02-11 23:32:53 -0300

    correcciones en sitterSlice

--- PawBnB/src/redux/sitterSlice.js
+++ PawBnB/src/redux/sitterSlice.js
@@ -1,56 +1,43 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice } from "@reduxjs/toolkit";
 
-export const fetchCurrentSitter = createAsyncThunk(
-  "sitter/fetchCurrentSitter",
-  async () => {
-    const response = await axios.get("/api/sitters/current");
-    return response.data;
-  }
-);
+const initialState = {
+  name: "",
+  surName: "",
+  phone: "",
+  address: "",
+  neighborhood: "",
+  city: "",
+  description: "",
+  rate: "",
+};
 
-export const updateSitter = createAsyncThunk("sitter/update", async (data) => {
-  const response = await axios.put("http://localhost:3000/sitters", data);
-  return response.data;
-});
-
-export const fetchSitterById = createAsyncThunk(
-  "sitter/fetchSitterById",
-  async (sitterId) => {
-    const response = await axios.get(`/api/sitters/${sitterId}`);
-    return response.data;
-  }
-);
-
-const sitterSlice = createSlice({
+export const sitterSlice = createSlice({
   name: "sitter",
-  initialState: {
-    currentSitter: null,
-    sitterDetail: null,
-  },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchSitterById.fulfilled, (state, action) => {
-        state.sitterDetail = action.payload;
-      })
-      .addCase(fetchCurrentSitter.fulfilled, (state, action) => {
-        state.currentSitter = action.payload;
-      });
-    builder
-      .addCase(fetchSitterById.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(fetchSitterById.fulfilled, (state, action) => {
-        state.loading = false;
-        state.sitterDetail = action.payload;
-      })
-      .addCase(fetchSitterById.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message;
-      });
+  initialState,
+  reducers: {
+    sitterInfo: (state, action) => {
+      const {
+        name,
+        surName,
+        phone,
+        address,
+        neighborhood,
+        city,
+        description,
+        rate,
+      } = action.payload;
+
+      state.name = name;
+      state.surName = surName;
+      state.phone = phone;
+      state.address = address;
+      state.neighborhood = neighborhood;
+      state.city = city;
+      state.description = description;
+      state.rate = rate;
+    },
   },
 });
 
-export default sitterSlice.reducer;          
+export const { sitterInfo } = sitterSlice.actions;
+export default sitterSlice.reducer;

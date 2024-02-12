0aaae4f9dc0927ab8d216c530f424c6abd91827f
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-02-12 20:28:41 -0300

    implementacion del login con google y agregado funcion de deslogueo

--- PawBnB/src/redux/authSlice.js
+++ PawBnB/src/redux/authSlice.js
@@ -27,8 +27,8 @@ const authSlice = createSlice({
         },
         logout(state) {
         state.isLoggedIn = false;
-        state.user = null;
-        state.role = null;
+        state.userId = null;
+        state.userRole = null;
         },
     },
 });
@@ -37,7 +37,8 @@ export const { loginStart, loginSuccess, loginFailure, logout } = authSlice.acti
 
 export const loginUser = (formData) => async (dispatch) => {
     try {
-    const response = await axios.post(`http://localhost:3001/login`, formData); 
+    const response = await axios.post(`http://localhost:3000/login`, formData); 
+    console.log(response.data)
         dispatch(loginSuccess(response.data)); 
     } catch (error) {
         if (error.response) {
@@ -47,6 +48,10 @@ export const loginUser = (formData) => async (dispatch) => {
             dispatch(loginFailure(error.message)); 
         }
     }
+};
+
+export const logOutUser = () => async (dispatch) => {
+    dispatch(logout())
 };
 
 export default authSlice.reducer;

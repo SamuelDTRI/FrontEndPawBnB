2f72ad3c6a641408620b124b36c549b2708bd786
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-02-12 21:03:04 -0300

    redireccionamiento en boton login, boton registrate y boton hazte cuidador

--- PawBnB/src/Components/NavBar/NavBar.jsx
+++ PawBnB/src/Components/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import SearchBar from "../SearchBar/SearchBar";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import logo from "../imagenes/logo/logo-pawbnb-horizontal.png";
 import style from "./NavBar.module.css";
@@ -10,9 +9,8 @@ import { logOutUser } from "../../redux/authSlice.js";
 const NavBar = () => {
   const {googleUser, googleLogOut} = UserAuth();
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const userRole = useSelector((state) => state.auth.userRole);
-  console.log(userRole)
 
   const handleSignOut = async ()=>{
     try {
@@ -26,6 +24,12 @@ const NavBar = () => {
       console.log("Error: ", error);
     }
   }
+  const handleSingUpRedir= ()=>{
+    navigate("/SignUp");
+  }
+  const handleLoginRedir = () => {
+    navigate("/Login");
+  };
   return (
     <div className="container-fluid">
       <nav className="navbar fixed-top border-bottom bg-white">
@@ -38,15 +42,25 @@ const NavBar = () => {
         </div>
 
         <div className="col-12 col-md-3 m-1">
-          {(googleUser||userRole)? (<button className="btn border-warning text-warning"
-                                  onClick={handleSignOut}>
-                                    LogOut</button>):
-          (<>
-            <button className="btn  me-2 border-warning text-warning">
-              Sign up
+          {googleUser || userRole ? (
+            <button
+              className="btn border-warning text-warning"
+              onClick={handleSignOut}>
+              LogOut
             </button>
-            <button className="btn border-warning text-warning">Login</button>
-          </>
+          ) : (
+            <>
+              <button
+                className="btn  me-2 border-warning text-warning"
+                onClick={handleSingUpRedir}>
+                Regístrate
+              </button>
+              <button
+                className="btn border-warning text-warning"
+                onClick={handleLoginRedir}>
+                Login
+              </button>
+            </>
           )}
         </div>
       </nav>

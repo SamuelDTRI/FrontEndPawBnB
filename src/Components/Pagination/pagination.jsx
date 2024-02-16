870a66da0856d0049d6bd468a718b2741fe9c0fb
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-02-16 12:28:17 -0300

    ajustes en la funcion nextHandler,y en la forma de llamar la funciones nextHandler y preHandler

--- src/Components/Pagination/pagination.jsx
+++ src/Components/Pagination/pagination.jsx
@@ -6,11 +6,11 @@ const Pagination = ({ pages, prevHandler, nextHandler, currentPage }) => {
         <ContainerPagination>
             {pages >=1 &&
                 <div className="pagination-btn">
-                    <button className={currentPage>=1? "btn-nav" : "btn-none"} onClick={prevHandler}>&lt; Anterior</button>
+                    <button className={currentPage>=1? "btn-nav" : "btn-none"} onClick={()=>prevHandler()}>&lt; Anterior</button>
                     <div className="pagination-pag">
                         <span className="pag">{currentPage+1}</span><span>de {pages+1}</span>
                     </div>
-                    <button className={currentPage<pages? "btn-nav" : "btn-none"} onClick={nextHandler}>Siguiente &gt;</button>
+                    <button className={currentPage<pages? "btn-nav" : "btn-none"} onClick={()=>nextHandler()}>Siguiente &gt;</button>
                 </div>
             }
         </ContainerPagination>

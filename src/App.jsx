1f922977e08d24fc1c046f7cc3d07aeeecfe10e4
Nicolas Soto <nicosoto18@hotmail.com>
2024-02-19 11:49:32 -0300

    Correcciones

--- src/App.jsx
+++ src/App.jsx
@@ -39,46 +39,51 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/SignUp" element={<SignUpOwners />} />
         <Route path="/SignUpSitters" element={<SignUpSitters />} />
-        <Route
-          path="/dashboardSitter/:id"
-          element={
-            !userDeleted && userRole === "DogSitter" && userId ? (
-              <DashboardSitter />
+        <Route path="/dashboardSitter/:id" element={ <DashboardSitter /> }/>
+            {/* !userDeleted && userRole === "DogSitter" && userId ? (
+             
             ) : (
               <Navigate to="/" />
             )
-          }
-        />
-        <Route
-          path="/dashboardOwner/:id"
-          element={
+          
+        */}
+
+        <Route path="/dashboardOwner/:id" element={<DashboardOwner />}/>
+{/*          
             !userDeleted && userRole === "Owner" && userId ? (
-              <DashboardOwner />
+              
             ) : (
               <Navigate to="/" />
             )
-          }
-        />
+           */}       
         <Route
-          path="/sitterProfile/:id"
-          element={
-            !userDeleted && userRole === "DogSitter" && userId ? (
-              <SitterProfile />
+          path="/sitterProfile/:id"element={<SitterProfile/> }/>          
+            {/* !userDeleted && userRole === "DogSitter" && userId ? (
+            
             ) : (
               <Navigate to="/" />
             )
-          }
-        />
+          */}
+        
         <Route
           path="/reservation"
           element={userId ? <ReservationRequest /> : <SignUpOwners />}
         />
         <Route path="/Login" element={<Login />} />
         <Route path="/Home" element={<Home />} />
-        {/* <Route path="/Home/:id" element={<Home />} />  */}
-        <Route path="/dashboardAdmin" element={<DashboardAdmin />} />
-        <Route path="/dashboardAdmin/users" element={<DashboardAdmin />} />
+
         <Route path="/admin/login" element={<AdminLogin />} />
+        <Route
+          path="/dashboardAdmin"
+          element={
+              <DashboardAdmin />
+          }
+        />
+        <Route
+          path="/dashboardAdmin/users"
+          element={
+              <DashboardAdmin />
+          }
         />
 
       </Routes>

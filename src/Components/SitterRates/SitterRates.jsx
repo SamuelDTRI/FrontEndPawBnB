f2606f2cad46e7bd61f0fadb23468b713ca76671
Candela <Dalmassoc29@gmail.com>
2024-02-26 23:12:51 -0300

    boton del perfil -reservar ahora- funcional

--- src/Components/SitterRates/SitterRates.jsx
+++ src/Components/SitterRates/SitterRates.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import styles from "./SitterRates.module.css";
+import { Link } from "react-router-dom";
 
 const SitterRates = ({ infoSitter }) => {
   return (
